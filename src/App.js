4449a4cb6b246ea68447859779428fbb6cd61bf7
MD RAKIBUL ISLAM RAKIB <rakibulinux@gmail.com>
2022-09-29 21:37:12 +0600

    blog possition fixed

--- src/App.js
+++ src/App.js
@@ -33,37 +33,38 @@ function App() {
             ></Home>
           ))}
         </div>
+        <div className="question text-black m-2 p-2">
+          <h2 className="text-2xl">How does react work?</h2>
+          <p className="mt-2">
+            ReactJS divides the UI into isolated reusable pieces of code known
+            as components. React components work similarly to JavaScript
+            functions as they accept arbitrary inputs called properties or
+            props. It's possible to have as many components as necessary without
+            cluttering your code.
+          </p>
+          <h2 className="text-2xl">
+            Difference between props and state in react?
+          </h2>
+          <p className="mt-2">
+            Props are used to pass data from one component to another. The state
+            is a local data storage that is local to the component only and
+            cannot be passed to other components. The this. setState property is
+            used to update the state values in the component.
+          </p>
+          <h2 className="text-2xl">UseEffect working area?</h2>
+          <p className="mt-2">
+            UseEffect is the sideEffect of React. By using this Hook, you tell
+            React that your component needs to do something after render. React
+            will remember the function you passed (we’ll refer to it as our
+            “effect”), and call it later after performing the DOM updates. In
+            this effect, we set the document title, but we could also perform
+            data fetching or call some other imperative API.
+          </p>
+        </div>
       </div>
 
       <div className="Activity bg-gray-200">
         <Activity></Activity>
-      </div>
-      <div className="question text-black m-2 p-2">
-        <h2 className="text-2xl">How does react work?</h2>
-        <p className="mt-2">
-          ReactJS divides the UI into isolated reusable pieces of code known as
-          components. React components work similarly to JavaScript functions as
-          they accept arbitrary inputs called properties or props. It's possible
-          to have as many components as necessary without cluttering your code.
-        </p>
-        <h2 className="text-2xl">
-          Difference between props and state in react?
-        </h2>
-        <p className="mt-2">
-          Props are used to pass data from one component to another. The state
-          is a local data storage that is local to the component only and cannot
-          be passed to other components. The this. setState property is used to
-          update the state values in the component.
-        </p>
-        <h2 className="text-2xl">UseEffect working area?</h2>
-        <p className="mt-2">
-          UseEffect is the sideEffect of React. By using this Hook, you tell
-          React that your component needs to do something after render. React
-          will remember the function you passed (we’ll refer to it as our
-          “effect”), and call it later after performing the DOM updates. In this
-          effect, we set the document title, but we could also perform data
-          fetching or call some other imperative API.
-        </p>
       </div>
     </div>
   );
