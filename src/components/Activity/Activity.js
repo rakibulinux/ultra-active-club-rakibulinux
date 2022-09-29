44de426c22a5a7d39cdedd10421b28b6ce8558f6
MD RAKIBUL ISLAM RAKIB <rakibulinux@gmail.com>
2022-09-29 23:31:02 +0600

    try to add break time

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import List from "../List/List";
 import "./Activity.css";
 
 const Activity = ({ studies }) => {
+  const [myAct, setMyAct] = useState([]);
+  console.log(myAct);
   return (
     <div className="Activity flex flex-col items-center">
       <List></List>
